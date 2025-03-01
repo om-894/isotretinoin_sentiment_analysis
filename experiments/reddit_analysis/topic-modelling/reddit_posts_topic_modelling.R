81bea767a46a8e663ed505eedfb292596e29e634
om-894 <158072423+om-894@users.noreply.github.com>
2025-03-01 15:39:50 +0000

    initial code for tokenization of post_body

--- experiments/reddit_analysis/topic-modelling/reddit_posts_topic_modelling.R
+++ experiments/reddit_analysis/topic-modelling/reddit_posts_topic_modelling.R
@@ -21,3 +21,23 @@ reddit_data <- read_csv("data-raw/reddit-posts-and-comments/all_subreddits_reddi
 df_combined <- reddit_data %>%
   group_by(subreddit, post_id, post_title, post_body) %>%  # Keep post id, title and text
   summarise(comments_combined = paste(comment, collapse = " "), .groups = "drop")
+
+# Tokenize the text and remove stopwords ---------------------------------------
+
+# Additional preprocessing
+df_tokens <- df_combined %>%
+  mutate(full_text = paste(post_title, post_body, comments_combined, sep = " ")) %>%
+  unnest_tokens(word, full_text) %>%
+  anti_join(stop_words, by = "word") %>%
+  filter(!str_detect(word, "^[0-9]+$")) %>%
+  filter(nchar(word) > 2) %>%
+  # Add these steps:
+  filter(!word %in% c("amp", "http", "https", "com", "www")) %>% # Remove common web artifacts
+  filter(!str_detect(word, "^[[:punct:]]+$")) %>% # Remove punctuation-only tokens
+  mutate(word = str_replace_all(word, "[[:punct:]]", "")) # Clean remaining punctuation
+
+
+
+
+
+
