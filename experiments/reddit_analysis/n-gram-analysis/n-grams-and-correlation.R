26eef79410ce244cb9d6797ca3a6b13ca6d61108
om-894 <158072423+om-894@users.noreply.github.com>
2025-03-03 12:16:06 +0000

    created new file for n-gram analysis

---
+++ experiments/reddit_analysis/n-gram-analysis/n-grams-and-correlation.R
@@ -0,0 +1,6 @@
+
+# An n-gram is a sequence of n adjacent symbols in particular order.
+
+# This script demonstrates how to analyze relationships between words using n-grams and correlations.
+# We will explore tokenizing text into bigrams, filtering and counting n-grams, analyzing bigrams,
+# using bigrams for sentiment analysis, visualizing networks of bigrams, and examining word correlations.
