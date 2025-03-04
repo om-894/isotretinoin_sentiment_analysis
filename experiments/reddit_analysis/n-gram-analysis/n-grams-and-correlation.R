5897c8976e3a2d6671ea44a4548a87b8b96f215a
om-894 <158072423+om-894@users.noreply.github.com>
2025-03-04 11:32:51 +0000

    tokenized text into bigrams

--- experiments/reddit_analysis/n-gram-analysis/n-grams-and-correlation.R
+++ experiments/reddit_analysis/n-gram-analysis/n-grams-and-correlation.R
@@ -5,7 +5,7 @@
 # We will explore tokenizing text into bigrams, filtering and counting n-grams, analyzing bigrams,
 # using bigrams for sentiment analysis, visualizing networks of bigrams, and examining word correlations.
 
-# Load necessary libraries
+# Load necessary libraries -----------------------------------------------------
 library(dplyr)
 library(tidytext)
 library(tidyr)
@@ -13,6 +13,7 @@ library(ggplot2)
 library(igraph)
 library(ggraph)
 library(widyr)
+library(tidyverse)
 
 # Load the dataset
 data <- read_csv("data-raw/reddit-posts-and-comments/all_subreddits_reddit_posts.csv")
@@ -22,3 +23,32 @@ data <- read_csv("data-raw/reddit-posts-and-comments/all_subreddits_reddit_posts
 df_combined <- data %>%
   group_by(subreddit, post_id, post_title, post_body) %>%  # Keep post id, title and text
   summarise(comments_combined = paste(comment, collapse = " "), .groups = "drop")
+
+# Tokenizing by N-gram
+# Tokenize the text into bigrams (pairs of consecutive words)
+comment_bigrams <- df_combined %>%
+  group_by(subreddit, post_body) %>%
+  unnest_tokens(bigram, comments_combined, token = "ngrams", n = 2)
+
+# Counting and Filtering N-grams -----------------------------------------------
+
+# Separate the bigrams into two columns
+bigrams_separated <- comment_bigrams %>%
+  separate(bigram, c("word1", "word2"), sep = " ")
+
+# Remove stop words from both words in the bigrams
+bigrams_filtered <- bigrams_separated %>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word)
+
+# Count the filtered bigrams
+bigram_counts <- bigrams_filtered %>%
+  count(word1, word2, sort = TRUE)
+
+bigram_counts
+
+
+
+
+
+
