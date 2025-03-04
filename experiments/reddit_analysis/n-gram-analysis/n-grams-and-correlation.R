a4e74fe1e4174ccf25fcb757fb33ea73fb33552f
om-894 <158072423+om-894@users.noreply.github.com>
2025-03-04 11:22:55 +0000

    load packages and load data

--- experiments/reddit_analysis/n-gram-analysis/n-grams-and-correlation.R
+++ experiments/reddit_analysis/n-gram-analysis/n-grams-and-correlation.R
@@ -4,3 +4,21 @@
 # This script demonstrates how to analyze relationships between words using n-grams and correlations.
 # We will explore tokenizing text into bigrams, filtering and counting n-grams, analyzing bigrams,
 # using bigrams for sentiment analysis, visualizing networks of bigrams, and examining word correlations.
+
+# Load necessary libraries
+library(dplyr)
+library(tidytext)
+library(tidyr)
+library(ggplot2)
+library(igraph)
+library(ggraph)
+library(widyr)
+
+# Load the dataset
+data <- read_csv("data-raw/reddit-posts-and-comments/all_subreddits_reddit_posts.csv")
+
+# I want to also keep the title of the post but just combine the comments. 
+# Combine comments for each post_id and keep the post title and text
+df_combined <- data %>%
+  group_by(subreddit, post_id, post_title, post_body) %>%  # Keep post id, title and text
+  summarise(comments_combined = paste(comment, collapse = " "), .groups = "drop")
