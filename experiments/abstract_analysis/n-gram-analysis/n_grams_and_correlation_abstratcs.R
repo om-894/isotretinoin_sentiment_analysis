9135eb72ab64baea1dc449b4ee83460e392f7bb5
om-894 <158072423+om-894@users.noreply.github.com>
2025-03-09 12:14:07 +0000

    load and manipulate data into two time periods

--- experiments/abstract_analysis/n-gram-analysis/n_grams_and_correlation_abstratcs.R
+++ experiments/abstract_analysis/n-gram-analysis/n_grams_and_correlation_abstratcs.R
@@ -14,3 +14,39 @@ library(igraph)
 library(ggraph)
 library(widyr)
 library(tidyverse)
+
+# Load data --------------------------------------------------------------------
+
+data <- read_csv("data-raw/isotretinoin_abstracts_supp.csv")
+
+# Filter rows where `has_abstract` is TRUE and remove missing abstracts
+abstracts_data <- data %>%
+  filter(has_abstract == TRUE, !is.na(abstract)) %>%
+  select(pmid, title, abstract, year)
+
+# SPLIT DATAFRAME WHEN CERTAIN MEASURES WERE INTRODUCED???
+# Dont have to split, just add a column for groups
+
+# What is the eariest year in the dataset?
+min(abstracts_data$year) # 1987
+
+# The latest year is 2024. We will split the data into 2006=< and 2006>
+# Fortunately, halfway in the time periods is 2006, which also was the introduction
+# of the iPLEDGE program and not long after reddit was founded.
+
+abstracts_data <- abstracts_data %>%
+  mutate(period = ifelse(year >= 2006, "2006 and later", "Before 2006"))
+
+
+# Tokenizing by N-gram ---------------------------------------------------------
+
+# Tokenize the text into bigrams (pairs of consecutive words)
+abstract_bigrams <- abstracts_data %>%
+  group_by(period, abstract) %>%
+  unnest_tokens(bigram, comments_combined, token = "ngrams", n = 2)
+
+
+
+
+
+
